5956a417ba82b89c720aa68fa9cfe8e9729d629c
zounas <peltex@hotmail.fi>
2025-04-04 22:22:40 +0300

    Website

---
+++ js/main.js
@@ -0,0 +1,59 @@
+ 
+ jQuery(document).ready(function() {
+	 var top = jQuery(document).scrollTop();
+		var height = 200;
+	  //alert(batas);
+	  
+	  if (top > height) {
+		jQuery('.navbar-fixed-top').addClass('menu-scroll');
+	  } else {
+	   jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+	  }
+	 
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	}); 
+	 
+
+	jQuery(window).scroll(function () {
+	  var top = jQuery(document).scrollTop();
+		var height = 200;
+	  //alert(batas);
+	  
+	  if (top > height) {
+		jQuery('.navbar-fixed-top').addClass('menu-scroll');
+	  } else {
+	   jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+	  }
+	}); 
+	
+//	 /*---------------------------------------------*
+//     * STICKY scroll
+//     ---------------------------------------------*/
+
+    $.localScroll();
+	
+	
+	
+	
+//    /*---------------------------------------------*
+//     * WOW
+//     ---------------------------------------------*/
+ 
+        var wow = new WOW({
+            mobile: false // trigger animations on mobile devices (default is true)
+       });
+ 	
+	 
+	 
+ });
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+ 
+			
